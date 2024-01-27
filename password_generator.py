726d86edd6fd7d8e19dd4a1d9b6ff9a8899ae3b7
MachariaP <walburphinehas78@gmail.com>
2024-01-27 12:03:20 +0300

    Random password generation script

---
+++ password_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Import necessary libraries
+import random
+import string
+
+# Prompt the user to input the desired password length
+password_len = int(input("Write password length: "))
+
+# Initialize an empty string to store the generated password
+my_password = ""
+
+# Loop to generate the password of the specified length
+for i in range(password_len):
+    my_password += random.choice(string.ascii_letters)
+
+# Print the generated password
+print("Your new password: ", my_password)
